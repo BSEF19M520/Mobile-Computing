f8aebf1a7c1f51a60484b3cf5828c048e07ef60c
BSEF19M520 <bsef19m520@pucit.edu.pk>
2022-04-30 17:03:19 +0500

    Ids Corrected, Portrait and Horizontal both orientations supported, Calculator activity fully implemented, Buttons to switch to Login, Register, Calculator activities added in Main Activity, Up action added to navigate back to Main Activity from other acitivies

---
+++ MyApplication/app/src/main/java/com/example/myapplication/CalculatorActivity.java
@@ -0,0 +1,122 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+import tk.pratanumandal.expr4j.ExpressionEvaluator;
+
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final ExpressionEvaluator evaluator = new ExpressionEvaluator();
+    private static final String INVALID = "Invalid Expression";
+    TextView expressionTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(com.google.android.material.R.style.Theme_AppCompat_Light);
+        setContentView(R.layout.activity_calculator);
+        super.onCreate(savedInstanceState);
+        this.expressionTextView = findViewById(R.id.calculatorExpressionTextView);
+        findViewById(R.id.calculatorClearAllButton).setOnClickListener(this);
+        findViewById(R.id.calculatorClearButton).setOnClickListener(this);
+        findViewById(R.id.calculator0Button).setOnClickListener(this);
+        findViewById(R.id.calculator1Button).setOnClickListener(this);
+        findViewById(R.id.calculator2Button).setOnClickListener(this);
+        findViewById(R.id.calculator3Button).setOnClickListener(this);
+        findViewById(R.id.calculator4Button).setOnClickListener(this);
+        findViewById(R.id.calculator5Button).setOnClickListener(this);
+        findViewById(R.id.calculator6Button).setOnClickListener(this);
+        findViewById(R.id.calculator7Button).setOnClickListener(this);
+        findViewById(R.id.calculator8Button).setOnClickListener(this);
+        findViewById(R.id.calculator9Button).setOnClickListener(this);
+        findViewById(R.id.calculatorAdditionButton).setOnClickListener(this);
+        findViewById(R.id.calculatorSubtractionButton).setOnClickListener(this);
+        findViewById(R.id.calculatorDivisionButton).setOnClickListener(this);
+        findViewById(R.id.calculatorModulusButton).setOnClickListener(this);
+        findViewById(R.id.calculatorMultiplyButton).setOnClickListener(this);
+        findViewById(R.id.calculatorPointButton).setOnClickListener(this);
+        findViewById(R.id.calculatorEqualsButton).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        String expression = "";
+        if (view.getId() == R.id.calculatorClearAllButton) {
+            expression = "";
+        } else if (view.getId() == R.id.calculatorClearButton) {
+            expression = this.trimRight(this.getExpression());
+        } else if (view.getId() == R.id.calculator0Button) {
+            expression = this.getExpression().concat("0");
+        } else if (view.getId() == R.id.calculator1Button) {
+            expression = this.getExpression().concat("1");
+        } else if (view.getId() == R.id.calculator2Button) {
+            expression = this.getExpression().concat("2");
+        } else if (view.getId() == R.id.calculator3Button) {
+            expression = this.getExpression().concat("3");
+        } else if (view.getId() == R.id.calculator4Button) {
+            expression = this.getExpression().concat("4");
+        } else if (view.getId() == R.id.calculator5Button) {
+            expression = this.getExpression().concat("5");
+        } else if (view.getId() == R.id.calculator6Button) {
+            expression = this.getExpression().concat("6");
+        } else if (view.getId() == R.id.calculator7Button) {
+            expression = this.getExpression().concat("7");
+        } else if (view.getId() == R.id.calculator8Button) {
+            expression = this.getExpression().concat("8");
+        } else if (view.getId() == R.id.calculator9Button) {
+            expression = this.getExpression().concat("9");
+        } else if (view.getId() == R.id.calculatorModulusButton) {
+            expression = this.getExpression().concat("%");
+        } else if (view.getId() == R.id.calculatorAdditionButton) {
+            expression = this.getExpression().concat("+");
+        } else if (view.getId() == R.id.calculatorMultiplyButton) {
+            expression = this.getExpression().concat("*");
+        } else if (view.getId() == R.id.calculatorSubtractionButton) {
+            expression = this.getExpression().concat("-");
+        } else if (view.getId() == R.id.calculatorDivisionButton) {
+            expression = this.getExpression().concat("/");
+        } else if (view.getId() == R.id.calculatorPointButton) {
+            expression = this.getExpression().concat(".");
+        } else if (view.getId() == R.id.calculatorEqualsButton) {
+            this.evaluateExpression();
+        }
+        this.setExpression(expression);
+    }
+
+    public void evaluateExpression() {
+        try {
+            double result = evaluator.evaluate(this.getExpression());
+            Toast toast = Toast.makeText(getApplicationContext(), String.valueOf(result), Toast.LENGTH_SHORT);
+            toast.show();
+            this.setExpression(Double.toString(result));
+        } catch (Exception ignored) {
+            this.setExpression(INVALID);
+            Toast toast = Toast.makeText(getApplicationContext(), INVALID, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    public String getExpression() {
+        return (String) this.expressionTextView.getText();
+    }
+
+    public void setExpression(String expression) {
+        this.expressionTextView.setText(this.createExpression(expression));
+    }
+
+    public String createExpression(String str) {
+        return this.getString(R.string.calculator_expressions, str);
+    }
+
+    public String trimRight(String str) {
+        if (str.length() > 0)
+            str = str.substring(0, str.length() - 1);
+        return str;
+    }
+}
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,9 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(com.google.android.material.R.style.Theme_AppCompat_Light);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
+        setContentView(R.layout.activity_main);
         textView = this.findViewById(R.id.mainActivityTextView);
-        String initialText = (String) textView.getText();
         final AtomicBoolean toggle = new AtomicBoolean(true);
         button = this.findViewById(R.id.mainActivityToggleButton);
         button.setOnClickListener(new View.OnClickListener() {
@@ -29,8 +29,35 @@ public class MainActivity extends AppCompatActivity {
                 if (toggle.getAndSet(!toggle.get()))
                     textView.setText("Text changed");
                 else
-                    textView.setText(initialText);
+                    textView.setText(R.string.toggle_text);
             }
         });
+
+        findViewById(R.id.mainActivityCalculatorActivityButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToActivity(CalculatorActivity.class);
+            }
+        });
+
+        findViewById(R.id.mainActivityLoginActivityButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToActivity(LoginActivity.class);
+            }
+        });
+
+        findViewById(R.id.mainActivityRegisterActivityButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchToActivity(RegisterActivity.class);
+            }
+        });
+
+    }
+
+    private void switchToActivity(Class clazz) {
+        Intent intent = new Intent(this, clazz);
+        this.startActivity(intent);
     }
 }
